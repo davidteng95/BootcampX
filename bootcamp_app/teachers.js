31778b8382382907d202befb21989243f4ee8775
David <davidteng95@gmail.com>
2023-06-29 05:03:53 +0000

    Node Postgres, pool.query practice

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,27 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+
+pool.query(`
+SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
+FROM assistance_requests
+JOIN teachers ON teachers.id = assistance_requests.teacher_id
+JOIN students ON students.id = assistance_requests.student_id
+JOIN cohorts ON cohorts.id = students.cohort_id
+WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+GROUP BY teachers.name, cohorts.name
+ORDER BY teacher;
+`)
+.then(res => {
+  console.log("connected");
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.teacher}`);
+  })
+}).catch(err => console.error('query error', err.stack));
+
